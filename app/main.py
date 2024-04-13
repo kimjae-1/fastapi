b9a2bbc48611f98d8ca69a410b455b7aaf0c3ad2
kimjae-1 <kimjae1@g.skku.edu>
2024-04-13 03:44:00 +0000

    healthcheck

--- app/main.py
+++ app/main.py
@@ -6,19 +6,22 @@ from app import routers
 from app.core.model_registry import model_registry
 from app.utils import load_model
 
+
 @asynccontextmanager
-async def lifespan(app : FastAPI):
+async def lifespan(app: FastAPI):
     model = await run_in_threadpool(load_model)
     model_registry.register_model("iris_model", model)
-    
+
     yield
-    
+
     model_registry.clear()
 
-app = FastAPI(lifespan = lifespan)
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(routers.router)
 
-@app.get("/healthcehck")
+
+@app.get("/healthcheck")
 async def healthcheck():
-    return {'status':'ok'}
+    return {"status": "ok"}
